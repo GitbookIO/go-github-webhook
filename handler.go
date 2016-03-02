b937da5d1fbdf54160a16ff0675bf023d44bfe40
Aaron O'Mullan <aaron.omullan@gmail.com>
2016-03-02 14:58:02 +0100

    Validate payload with signature only if secret is provided, fixes #1
    Move signature header validation into separate func (more readable)

--- handler.go
+++ handler.go
@@ -36,30 +36,20 @@ func Handler(secret string, fn WebhookHandler) http.Handler {
 			return
 		}
 
-		// Ensure secret is a sha1 hash
-		signature_parts := strings.SplitN(signature, "=", 2)
-		if len(signature_parts) != 2 {
-			_fail(fmt.Errorf("Signature: '%s' does not contain two parts (hash type and hash)", signature))
-			return
-		}
-		signature_type := signature_parts[0]
-		signature_hash := signature_parts[1]
-		if signature_type != "sha1" {
-			_fail(fmt.Errorf("Signature should be a 'sha1' hash not '%s'", signature_type))
-			return
-		}
-
+		// Read body
 		body, err := ioutil.ReadAll(req.Body)
 		if err != nil {
 			_fail(err)
 			return
 		}
 
-		// Check that payload came from github
-		// skip check if empty secret provided
-		if secret != "" && !IsValidPayload(secret, signature_hash, body) {
-			_fail(fmt.Errorf("Payload did not come from GitHub"))
-			return
+		// Validate payload (only when secret is provided)
+		if secret != "" {
+			if err := validePayloadSignature(secret, signature, body); err != nil {
+				// Valied validation
+				_fail(err)
+				return
+			}
 		}
 
 		// Get payload
@@ -76,6 +66,29 @@ func Handler(secret string, fn WebhookHandler) http.Handler {
 			_fail(err)
 		}
 	})
+}
+
+func validePayloadSignature(secret, signatureHeader string, body []byte) error {
+	// Check header is valid
+	signature_parts := strings.SplitN(signatureHeader, "=", 2)
+	if len(signature_parts) != 2 {
+		return fmt.Errorf("Invalid signature header: '%s' does not contain two parts (hash type and hash)", signatureHeader)
+	}
+
+	// Ensure secret is a sha1 hash
+	signature_type := signature_parts[0]
+	signature_hash := signature_parts[1]
+	if signature_type != "sha1" {
+		return fmt.Errorf("Signature should be a 'sha1' hash not '%s'", signature_type)
+	}
+
+	// Check that payload came from github
+	// skip check if empty secret provided
+	if !IsValidPayload(secret, signature_hash, body) {
+		return fmt.Errorf("Payload did not come from GitHub")
+	}
+
+	return nil
 }
 
 func succeed(w http.ResponseWriter, event string) {
